80f852d37ad9249b339e3fa3d7dc28611613cfd7
LucasStoppa <134460964+LucasStoppa@users.noreply.github.com>
2023-11-27 20:08:11 -0300

    Add files via upload

---
+++ Endereco.java
@@ -0,0 +1,32 @@
+public class Endereco {
+    private String rua;
+    private String cidade;
+    private String estado;
+
+       public Endereco() {
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+}
